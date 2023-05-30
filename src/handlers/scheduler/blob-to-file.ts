8f6167801e7bfbaeb4d4b8b42e22580f1dc11aa1
cobategit <mancunianmerchant816@gmail.com>
2023-05-30 12:01:38 +0700

    scheduler

--- src/handlers/scheduler/blob-to-file.ts
+++ src/handlers/scheduler/blob-to-file.ts
@@ -3,7 +3,7 @@ import shelljs from 'shelljs'
 import path from 'path'
 
 export const taskBlobToFile = cronNode.schedule(
-    `30 * * * *`,
+    `10 * * * *`,
     () => {
         if (shelljs.exec(`node ${path.join(process.cwd(), 'dist/services/crontab/send-file-to-ftp.js')}`).code !== 0) {
             console.log(`terjadi kesalahan cron job convert blob to file and send to ftp storage`)
